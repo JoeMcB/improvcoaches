be4e5c6e309890e9bfbe6381fa666e8502bbcf26
Joe Burns <joseph.burns@gmail.com>
2019-03-10 12:09:02 -0400

    BUG: Fix password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,27 +1,28 @@
+# frozen_string_literal: true
+
 class PasswordResetsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
-  	user = User.find_by_email(params[:email])
-  	user.send_password_reset if user
+    user = User.find_by(email: params[:email])
+    user&.send_password_reset
 
-  	redirect_to root_url, notice: "An email has been sent with a link to reset your password."
+    redirect_to root_url, notice: 'An email has been sent with a link to reset your password.'
   end
 
   def edit
-  	@user = User.find_by_password_reset_token!(params[:id])
+    @user = User.find_by!(password_reset_token: params[:id])
   end
 
   def update
-  	@user = User.find_by_password_reset_token!(params[:id])
+    @user = User.find_by!(password_reset_token: params[:id])
 
-  	if(@user.password_reset_time < 2.hours.ago)
-  	  redirect_to new_password_reset_path, alert: "Password reset link has expired."
-  	elsif @user.update_attributes(params[:user])
-  		redirect_to login_path, notice: "Your password has been updated."
-  	else
-  		render :edit
-  	end
+    if @user.password_reset_time < 2.hours.ago
+      redirect_to new_password_reset_path, alert: 'Password reset link has expired.'
+    elsif @user.update(params[:user].permit(:password, :password_confirmation))
+      redirect_to login_path, notice: 'Your password has been updated.'
+    else
+      render :edit
+    end
   end
 end
