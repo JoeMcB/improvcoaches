58ec2d15fbb9ec310e81343ec4f49ed4e9af4a23
Joe Burns <joseph.burns@gmail.com>
2024-04-19 11:50:44 -0500

    Fix Paperclip

--- config/application.rb
+++ config/application.rb
@@ -15,16 +15,5 @@ module Improvcoaches
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-        # paperclip
-        config.paperclip_defaults = {
-          storage: :s3,
-          s3_credentials: {
-            bucket: ENV['AWS_BUCKET'],
-            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-            s3_region: ENV['AWS_REGION']
-          }
-        }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,15 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.default_url_options = { :host => "www.improvcoaches.com" }
+
+  # paperclip
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_region: ENV['AWS_REGION']
+    }
+  }
 end
