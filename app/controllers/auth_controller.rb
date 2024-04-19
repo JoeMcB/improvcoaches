7d66d4672cd3fd73618a4d6a722b360cbfa3fadf
Joe Burns <joseph.burns@gmail.com>
2024-04-19 14:50:27 -0500

    Fix Auth logic

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -9,114 +9,99 @@ class AuthController < ApplicationController
 
   def confirm_link
     auth = session[:omniauth]
-    user = User.find_by_email auth[:info][:email].downcase
-    return_url = session.delete(:redirect_on_login) || :back
+    user = User.find_by_email(auth[:info][:email].downcase)
 
-    if !(user && user.authenticate(params[:password]))
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.oauth_token = auth.credentials.token
-      user.oauth_token_expires_at = Time.at(auth.credentials.expires_at)
+    unless user && user.authenticate(params[:password])
+      user.assign_attributes(
+        provider: auth[:provider],
+        uid: auth[:uid],
+        oauth_token: auth[:credentials][:token],
+        oauth_token_expires_at: Time.at(auth[:credentials][:expires_at])
+      )
 
       if user.save
         set_authorized_user(user, true)
-
-        respond_to do |format|
-          format.html { redirect_to return_url, flash: { notice: 'Your Facebook account has been linked.' } }
-          format.js { render partial: 'create' }
-        end
+        flash[:notice] = 'Your Facebook account has been linked.'
+        redirect_back(fallback_location: root_path)
       else
-        respond_to do |format|
-          format.html { redirect_to return_url, flash: { error: 'Your Facebook account could not be linked.' } }
-        end
+        flash[:error] = 'Your Facebook account could not be linked.'
+        redirect_back(fallback_location: root_path)
       end
     else
-      respond_to do |format|
-        format.html { redirect_to link_path, flash: { warning: 'Hmm, that email and password appear to be invalid.' } }
-      end
+      flash[:warning] = 'Hmm, that email and password appear to be invalid.'
+      redirect_to link_path
     end
   end
 
   def create
-    return_url = session.delete(:redirect_on_login) || :back
-
     if params['from_facebook']
       auth = request.env['omniauth.auth']
 
       if User.where(email: auth.info.email.downcase, provider: nil).first
         session[:omniauth] = auth.except('extra')
-        respond_to do |format|
-          format.html { redirect_to link_path, flash: { notice: 'We already have an account for that email.  You can link your accounts below.' } }
-          format.js { render partial: 'create' }
-        end
+        flash[:notice] = 'We already have an account for that email. You can link your accounts below.'
+        redirect_to link_path
       else
-        uid = auth['uid']
-        provider = auth['provider']
-        puts auth.inspect
-        User.where(uid: uid, provider: provider).first_or_initialize.tap do |user|
-          if user.id.nil?
-            user.provider = auth.provider
-            user.uid = auth.uid
-            user.name = auth.info.name
-            user.avatar = URI.parse(process_uri("http://graph.facebook.com/v10.0/#{auth.uid}/picture?height=500&width=500"))
-            user.email = auth.info.email
-            user.password_digest = SecureRandom.urlsafe_base64
-          end
+        user = User.where(uid: auth['uid'], provider: auth['provider']).first_or_initialize
+        setup_new_user(user, auth) unless user.persisted?
 
-          user.oauth_token = auth.credentials.token
-          user.oauth_token_expires_at = Time.at(auth.credentials.expires_at)
-
-          if !user.save
-            session[:omniauth] = auth.except('extra')
-            # action = { action: :link } #this is here because it was throwing an error down there (?)
-            redirect_to return_url, flash: { notice: 'Welp, no one should ever see this.  Email support if you do!' }
-            return false
-          else
-            set_authorized_user(user, true)
-
-            if params[:invite_code]
-              redirect_to invite_accept_url(code: params[:invite_code])
-            else
-              respond_to do |format|
-                format.html { redirect_to return_url, flash: { success: "Welcome back #{user.name}" } }
-                format.js { render partial: 'create' }
-              end
-            end
-          end
+        if user.save
+          set_authorized_user(user, true)
+          redirect_to_next_step(user, params[:invite_code])
+        else
+          session[:omniauth] = auth.except('extra')
+          flash[:notice] = 'Welp, no one should ever see this. Email support if you do!'
+          redirect_back(fallback_location: root_path)
         end
       end
     else
       user = User.find_by_email(params[:email].downcase)
-
-      if user.nil? || !(user && user.authenticate(params[:password]))
-        respond_to do |format|
-          format.html { redirect_to login_url, flash: { notice: 'Hmm, that email and password appear to be invalid.' } }
-          format.js { render partial: 'create' }
-        end
-      else
-        set_authorized_user(user, params[:remember_me])
-
-        if params[:invite_code]
-          session[:redirect_on_login] = invite_accept_url(code: params[:invite_code])
-        else
-          respond_to do |format|
-            format.html { redirect_to return_url, flash: { success: "Welcome back #{user.name}" } }
-            format.js { render partial: 'create' }
-          end
-        end
-      end
+      handle_existing_user(user, params)
     end
   end
 
   def destroy
-    return_url = session.delete(:redirect_on_login) || :back
     remove_authorized_user
-    redirect_to return_url, notice: 'You have been logged out.  See ya!'
+    flash[:notice] = 'You have been logged out. See ya!'
+    redirect_back(fallback_location: login_url)
   end
 
   def failure
-    return_url = session.delete(:redirect_on_login) || :back
-    message = params[:message] || ''
-    redirect_to return_url, flash: { notice: "There was an error logging you in via Facebook: #{message} " }
+    flash[:notice] = "There was an error logging you in via Facebook: #{params[:message] || ''}"
+    redirect_back(fallback_location: login_url)
+  end
+
+  private
+
+  def setup_new_user(user, auth)
+    user.attributes = {
+      provider: auth.provider,
+      uid: auth.uid,
+      name: auth.info.name,
+      email: auth.info.email,
+      avatar: URI.parse(process_uri("http://graph.facebook.com/v10.0/#{auth.uid}/picture?height=500&width=500")),
+      password_digest: SecureRandom.urlsafe_base64,
+      oauth_token: auth.credentials.token,
+      oauth_token_expires_at: Time.at(auth.credentials.expires_at)
+    }
+  end
+
+  def redirect_to_next_step(user, invite_code)
+    if invite_code
+      redirect_to invite_accept_url(code: invite_code), flash: { success: "Welcome back #{user.name}" }
+    else
+      flash[:success] = "Welcome back #{user.name}"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def handle_existing_user(user, params)
+    if user.nil? || !user.authenticate(params[:password])
+      flash[:notice] = 'Hmm, that email and password appear to be invalid.'
+      redirect_to login_url
+    else
+      set_authorized_user(user, params[:remember_me])
+      redirect_to_next_step(user, params[:invite_code])
+    end
   end
 end
