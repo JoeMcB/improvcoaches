1cbdd04b7570c95666a4e94802ea4a0f06ed52a8
Joe Burns <joseph.burns@gmail.com>
2024-04-22 18:16:59 -0400

    Fix avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def avatar_image(user, size = '300x300')
     if user.avatar_new.attached?
-      url_for(user.avatar_new.variant(resize: size))
+      url_for(user.avatar_new.variant(resize_to_fill: size))
     else
-      size == '300x300' ? '/assets/users/missing_medium.png' : '/assets/users/missing_thumb.png'
+      size == '300x300' ? asset_path('users/missing_medium.png') : asset_path('users/missing_thumb.png')
     end
   end
 end
