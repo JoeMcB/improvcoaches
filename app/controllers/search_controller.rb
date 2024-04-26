48b7456b0a2e9239f987e8fde8fbd6ddd859f8e5
Joe Burns <joseph.burns@gmail.com>
2024-04-26 09:48:44 -0400

    Fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,12 @@
 class SearchController < ApplicationController
   def search
     @searched = (params[:commit] == "Search" || params[:page]) #Did a search happen
-    
+
     if(@searched)
         user_ids = Array.new
 
         if(params[:commit] == "Search") #Fresh Search
-            
+
             if(params[:set] == "favorites")
                 users = current_user.bookmarked_users.is_coach
             elsif(params[:set] == "recommended")
@@ -33,7 +33,7 @@ class SearchController < ApplicationController
                 puts "lol"
                 users = users.joins(:experiences).where( "experiences.experience_type_id IN (?)", params[:experience_types])
             end
-            
+
 
             if(!users.empty?)
                 filtered_user_ids = users.uniq.pluck("users.id")
@@ -45,12 +45,14 @@ class SearchController < ApplicationController
                     time_block[:hour] = params[:start_hour] unless params[:start_hour].empty?
                     time_block[:minute] = params[:start_minute] unless ( params[:start_minute].nil? || params[:start_minute].empty?)
 
-                    schedules = Schedule.where( 
-                        "user_id IN ('"+filtered_user_ids.join("', '")+"')"
-                    ).joins(:time_blocks).where( time_blocks: time_block).group("schedules.id").each do |schedule|
-                        
+                    schedules = Schedule.where(user_id: filtered_user_ids)
+                                        .joins(:time_blocks)
+                                        .where(time_blocks: time_block)
+                                        .group("schedules.id")
+                                        .each do |schedule|
+
                         if(!time_block[:hour].nil?)
-                            
+
                             if(schedule.has_time_span(
                                 params[:start_hour] ? params[:start_hour].to_i : 0,
                                 params[:start_minute] ? params[:start_minute].to_i : 0,
@@ -58,20 +60,20 @@ class SearchController < ApplicationController
                                 params[:end_minute] ? params[:end_minute].to_i : nil,
                                 params[:day]
                             )) then
-                              user_ids << schedule.user_id     
+                              user_ids << schedule.user_id
                             end
                         else
-                            user_ids << schedule.user_id     
+                            user_ids << schedule.user_id
                         end
 
-                    end     
+                    end
                 else
                     user_ids = users.collect do |u|
                         u.id
                     end
                 end
-                
-               
+
+
 
                 session[:last_search_user_ids] = user_ids
             end
@@ -87,7 +89,7 @@ class SearchController < ApplicationController
             else  #Sort by schedule update
                 @users = @users.joins(:schedule).order("schedules.updated_at DESC")
             end
-            
+
             @users = @users.paginate( per_page: 10, page: params[:page] )
         end
     end
